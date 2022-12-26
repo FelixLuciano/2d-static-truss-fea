178be78b87b054bea6d44911ba71464761e7d2fb
FelixLuciano <felix.lucianodias@gmail.com>
2022-12-26 00:28:21 -0300

    refactor: :recycle: young's modulus instead of elasticity

--- src/material.py
+++ src/material.py
@@ -2,12 +2,12 @@ class Material:
     elasticity: float
     area: float
 
-    def __init__(self, elasticity: float, area: float):
-        self.elasticity = elasticity
+    def __init__(self, youngs_modulus: float, area: float):
+        self.elasticity = youngs_modulus
         self.area = area
 
-    def set_elasticity(self, elasticity: float):
-        self.elasticity = elasticity
+    def set_elasticity(self, youngs_modulus: float):
+        self.elasticity = youngs_modulus
 
     def set_area(self, area: float):
         self.area = area
